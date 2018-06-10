db5acd7c3f19b2fc94c38c8d6806167fb192de28
x1n3f <nurzynski@gmail.com>
2018-06-10 21:43:12 +0200

    Strona 41 ksiazki. Wygenerowano: View "Details" z controllerem Auctions i defaultowym Modelem (Auction.cs) z na sztywno wpisanym wierszem.

---
+++ Ebuy.Website/Controllers/AuctionsController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ebuy.Website.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        // GET: Auctions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Auctions/Details/5
+        public ActionResult Details(long id = 0)
+        {
+            var auction = new Ebuy.Website.Models.Auction
+            {
+                Id = id,
+                Title = "Brand new widged 2.0",
+                Description = "This is a brand new version 2.0 Widged",
+                StartPrice = 1.00m,
+                CurrentPrice = 13.40m,
+                StartTime = DateTime.Parse("2008-05-01 7:34:42Z"),
+                EndTime = DateTime.Parse("2008-05-01 7:34:42Z"),
+            };
+
+            return View(auction);
+        }
+
+        // GET: Auctions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Auctions/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Auctions/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Auctions/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Auctions/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Auctions/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
